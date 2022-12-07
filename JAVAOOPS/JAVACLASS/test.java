4c22ea2df85636682bc39ff23971cd7778a6ae32
Piyush Dubey <89024653+EngineerPiyush@users.noreply.github.com>
2022-12-07 16:56:49 +0530

    test.java

---
+++ JAVAOOPS/JAVACLASS/test.java
@@ -0,0 +1,14 @@
+public class test
+{
+int rollNo=88;;
+String name="Piyush";
+void display()
+{
+System.out.print("Roll Number = "+" "+rollNo+"Name = "+name);
+}
+public static void main(String []val)
+{
+test ob1=new test();
+ob1.display();
+}
+} 
